d9afd6c5270351a81b0dda48f961181044a0b3e7
Amit Yaffe <aayaffe@gmail.com>
2016-10-07 21:26:33 +0000

    Added npm package.json

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var url = require("url")
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/*', function (req, res) {
+  
+  var u = url.parse(req.url.toString())
+  var path = decodeURIComponent(u.path).replace('/','')
+  console.log(path.toString())
+  var d = Date.parse(path)
+  if (isNaN(d)) res.end('Not a date!')
+  else res.end (d.toDateString())
 });
 
 app.listen(process.env.PORT, function () {
