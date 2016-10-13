b7067757495e1edf244461241e07f6c4cbec23b2
Amit Yaffe <aayaffe@gmail.com>
2016-10-13 22:47:11 +0300

    Added certificate update time

--- PythonRMStoGoogleSheets/parseRMS.py
+++ PythonRMStoGoogleSheets/parseRMS.py
@@ -9,14 +9,14 @@ columns = [('Sail nr.', 1, 'SAILNUMB', 'A'),('Name', 2, 'NAME', 'B'),('Class', 3
            ('Owner', 5, 'OWNER', 'D'), ('Type', 6, 'TYPE', 'E'),
            ('Year', 7, 'YEAR', 'F'), ('LOA', 8, 'LOA', 'G'),
            ('CDL', 9, 'CDL', 'H'), ('TxtInsh.', 10, 'TMF', 'I'),
-           ('TxtOffsh.', 11, 'TMF-OF', 'J')]
+           ('TxtOffsh.', 11, 'TMF-OF', 'J'),('Update.', 12, 'DD_MM_yyYY HH:MM:SS', 'K')]
 
 def add_date(wks):
     wks.update_cell(1, len(columns)+3, "Updated:")
     wks.update_cell(1, len(columns) + 4, time.strftime("%d/%m/%y, %H:%M:%S"))
 
 def addHeader(wks):
-    cells = wks.range('A1:J1') #TODO: find largest column
+    cells = wks.range('A1:K1') #TODO: find largest column
     for cell,t in zip(cells,columns):
         cell.value = t[0]
     wks.update_cells(cells)
@@ -49,7 +49,7 @@ def add_class(wks, cdl1, cdl2):
     wks.update_cells(cell_list)
 
 
-widths = [(17, 29), (29, 53), (53, 71), (107, 112), (148, 184), (292, 299), (346, 354), (1267, 1275), (1306, 1315)]
+widths = [(17, 29), (29, 53), (53, 71), (107, 112), (148, 184), (292, 299), (346, 354), (1267, 1275), (1306, 1315), (272,292)]
 data = pd.read_fwf("http://data.orc.org/public/WPub.dll?action=DownRMS&CountryId=ISR",
                    colspecs=widths, header='infer', encoding='utf_8')
 
@@ -61,5 +61,5 @@ wks = gc.open_by_url(
     'https://docs.google.com/spreadsheets/d/1S0zaKu3JjvFwz585dRicAc9pjVrE7ghI2wjSeAdrjSs/edit?usp=sharing').sheet1
 addHeader(wks)
 addYachts(wks, data)
-add_class(wks, 8.5, 9.5)
+add_class(wks, 8.0, 9.0)
 add_date(wks)
